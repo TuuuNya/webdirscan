a413e158f6a8809c58afa714d70dd89c9cab0c59
Striker <yuzesheji@qq.com>
2016-03-29 18:29:17 +0800

    最简单的Web目录扫描工具

---
+++ webdirscan.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+import argparse
+import requests
+
+# 命令行传值
+parser = argparse.ArgumentParser()
+parser.add_argument('website',help="Website for scan, eg: http://www.secbox.cn | www.secbox.cn",type=str)
+args = parser.parse_args()
+
+# 字典设置
+webdic = 'dict/dict.txt'
+
+# 对输入的网址进行处理
+website = args.website
+
+# 请求头设置
+headers = {
+    'Accept': '*/*',
+    'Referer': website,
+    'User-Agent': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.1; ',
+    'Connection': 'Keep-Alive',
+    'Cache-Control': 'no-cache',
+}
+
+# 字典存入数组
+webdict = []
+
+with open(webdic) as infile:
+    while True:
+        dirdic = infile.readline().strip()
+        if(len(dirdic) == 0): break
+        webdict.append(website+dirdic)
+
+
+for url in webdict:
+    try:
+        respon = requests.get(url, headers=headers)
+    except Exception,e:
+        print url
+        print e
+
+    if(respon.status_code == 200):
+        print '['+str(respon.status_code)+']' + ":" + url
+
