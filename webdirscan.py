1b405c632716da946b9d6cd1b73ac835f58fa114
striker <yuzesheji@qq.com>
2016-04-03 03:55:35 +0800

    整体进行的修改,添加了多线程

--- webdirscan.py
+++ webdirscan.py
@@ -2,8 +2,11 @@
 #-*- coding: utf-8 -*-
 
 import re
+import sys
 import argparse
 import requests
+import threading
+import Queue
 from termcolor import colored
 
 # 版权区域
@@ -20,49 +23,86 @@ mycopyright = '''
 '''
 print colored(mycopyright,'cyan')
 
-# 命令行传值
-parser = argparse.ArgumentParser()
-parser.add_argument('website',help="Website for scan, eg: http://www.secbox.cn | www.secbox.cn",type=str)
-args = parser.parse_args()
+class Dirscan:
+    def __init__(self,website,dict,threads_num,output):
+        print 'Scan starting...'
+        self.website = website
+        self.dict = dict
+        self.threads_num = threads_num
+        self.output = website.replace('https://','').replace('http://','') + '.txt'
+        self._loadDict()
+        self._formatWebsite()
+        self._loadHeaders()
+        self._analysis404()
+        self.lock = threading.Lock()
 
-# 字典设置
-webdic = 'dict/dict.txt'
+    def _formatWebsite(self):
+        pattern = re.compile(r'^[http\:\/\/|https\:\/\/]')
+        res = pattern.match(self.website)
 
-# 对输入的网址进行处理
-website = args.website
-pattern = re.compile(r'^[http\:\/\/|https\:\/\/]')
-res = pattern.match(website)
+        if not (res):
+            self.website = 'http://' + self.website
 
-if not(res):
-    website = 'http://' + website
+    def _analysis404(self):
+        notfoundpage = requests.get(self.website + '/songgeshigedashuaibi/hello.html', allow_redirects=False)
+        self.notfoundpagetext = notfoundpage.text.replace('/songgeshigedashuaibi/hello.html', '')
 
-# 请求头设置
-headers = {
-    'Accept': '*/*',
-    'Referer': website,
-    'User-Agent': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.1; ',
-    'Cache-Control': 'no-cache',
-}
+    def _loadDict(self):
+        self.urllist = Queue.Queue()
+        with open(self.dict) as infile:
+            for line in infile.readlines():
+                self.urllist.put(line.strip())
 
-# 字典存入数组
-webdict = []
+    def _loadHeaders(self):
+        self.headers = {
+            'Accept': '*/*',
+            'Referer': self.website,
+            'User-Agent': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.1; ',
+            'Cache-Control': 'no-cache',
+        }
 
-with open(webdic) as infile:
+    def _scan(self):
+        url = self.website + self.urllist.get()
+        try:
+            respon = requests.get(url, headers=self.headers, timeout=60, allow_redirects=False)
+        except Exception, e:
+            print e
+
+        if (respon.status_code == 200 and respon.text != self.notfoundpagetext):
+            print colored('[' + str(respon.status_code) + ']', 'green') + " " + url
+            self.lock.acquire()
+            with open(self.output,'a+') as infile:
+                infile.write(url + '\n')
+            self.lock.release()
+        self.urllist.task_done()
+
+    def run(self):
+        for i in range(self.threads_num):
+            t = threading.Thread(target=self._scan,name=str(i))
+            t.setDaemon(True)
+            t.start()
+
+if __name__ == '__main__':
+    # 命令行传值
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-w','--website',help="Website for scan, eg: http://www.secbox.cn | www.secbox.cn", type=str)
+    parser.add_argument('-d','--dict',default="dict/dict.txt",help="Dict for scan, eg: url.txt | ./secbox_url.txt",type=str)
+    parser.add_argument('-t','--threads',dest="threads_num",default=20,help="Threads num for scan eg: 50 | 100",type=int)
+    parser.add_argument('-o','--output',help="Result of the webdirscan output",type=str)
+    args = parser.parse_args()
+
+    if(len(sys.argv) == 1):
+        parser.print_usage()
+        sys.exit()
+
+    d = Dirscan(args.website,args.dict,args.threads_num,args.output)
+    d.run()
+
+    # 判断线程如果全部结束就退出程序
     while True:
-        dirdic = infile.readline().strip()
-        if(len(dirdic) == 0): break
-        webdict.append(website+dirdic)
+        if(threading.activeCount() <= 1):
+            break
+        else:
+            pass
 
-# 404页面分析,避免有的网站所有页面都返回200的情况
-notfoundpage = requests.get(website+'/songgeshigedashuaibi/hello.html',allow_redirects=False)
-notfoundpagetext = notfoundpage.text.replace('/songgeshigedashuaibi/hello.html','')
-
-# 遍历扫描过程
-for url in webdict:
-    try:
-        respon = requests.get(url, headers=headers,timeout=30,allow_redirects=False)
-    except Exception,e:
-        print e
-
-    if(respon.status_code == 200 and respon.text != notfoundpagetext):
-        print colored('['+str(respon.status_code)+']','green') + " " + url
+    print 'Scan End!'
