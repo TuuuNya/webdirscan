b08d5f5b24e7f13ab3bb11039df9f72feb4cd8f8
byg0ne <byg0ne@163.com>
2016-04-14 10:52:01 +0800

    简单修改了下代码格式

--- webdirscan.py
+++ webdirscan.py
@@ -21,15 +21,15 @@ mycopyright = '''
 
 *****************************************************
 '''
-print colored(mycopyright,'cyan')
+print colored(mycopyright, 'cyan')
 
-class Dirscan:
-    def __init__(self,website,dict,threads_num,output):
+class Dirscan(object):
+    def __init__(self, website, dic, threads_num, output):
         print 'Scan starting...'
         self.website = website
-        self.dict = dict
+        self.dic = dic
         self.threads_num = threads_num
-        self.output = website.replace('https://','').replace('http://','') + '.txt'
+        self.output = website.replace('https://', '').replace('http://', '') + '.txt'
         self._loadDict()
         self._formatWebsite()
         self._loadHeaders()
@@ -40,7 +40,7 @@ class Dirscan:
         pattern = re.compile(r'^[http\:\/\/|https\:\/\/]')
         res = pattern.match(self.website)
 
-        if not (res):
+        if not res:
             self.website = 'http://' + self.website
 
     def _analysis404(self):
@@ -49,7 +49,7 @@ class Dirscan:
 
     def _loadDict(self):
         self.urllist = Queue.Queue()
-        with open(self.dict) as infile:
+        with open(self.dic) as infile:
             for line in infile.readlines():
                 self.urllist.put(line.strip())
 
@@ -68,41 +68,39 @@ class Dirscan:
         except Exception, e:
             print e
 
-        if (respon.status_code == 200 and respon.text != self.notfoundpagetext):
+        if respon.status_code == 200 and respon.text != self.notfoundpagetext:
             print colored('[' + str(respon.status_code) + ']', 'green') + " " + url
             self.lock.acquire()
-            with open(self.output,'a+') as infile:
+            with open(self.output, 'a+') as infile:
                 infile.write(url + '\n')
             self.lock.release()
         self.urllist.task_done()
 
     def run(self):
         for i in range(self.threads_num):
-            t = threading.Thread(target=self._scan,name=str(i))
+            t = threading.Thread(target=self._scan, name=str(i))
             t.setDaemon(True)
             t.start()
 
 if __name__ == '__main__':
     # 命令行传值
     parser = argparse.ArgumentParser()
-    parser.add_argument('-w','--website',help="Website for scan, eg: http://www.secbox.cn | www.secbox.cn", type=str)
-    parser.add_argument('-d','--dict',default="dict/dict.txt",help="Dict for scan, eg: url.txt | ./secbox_url.txt",type=str)
-    parser.add_argument('-t','--threads',dest="threads_num",default=20,help="Threads num for scan eg: 50 | 100",type=int)
-    parser.add_argument('-o','--output',help="Result of the webdirscan output",type=str)
+    parser.add_argument('-w', '--website', help="Website for scan, eg: http://www.secbox.cn | www.secbox.cn", type=str)
+    parser.add_argument('-d', '--dict', default="dict/dict.txt", help="Dict for scan, eg: url.txt | ./secbox_url.txt", type=str)
+    parser.add_argument('-t', '--threads', dest="threads_num", default=20, help="Threads num for scan eg: 50 | 100", type=int)
+    parser.add_argument('-o', '--output', help="Result of the webdirscan output", type=str)
     args = parser.parse_args()
 
-    if(len(sys.argv) == 1):
+    if len(sys.argv) == 1:
         parser.print_usage()
         sys.exit()
 
-    d = Dirscan(args.website,args.dict,args.threads_num,args.output)
+    d = Dirscan(args.website, args.dict, args.threads_num, args.output)
     d.run()
 
     # 判断线程如果全部结束就退出程序
     while True:
-        if(threading.activeCount() <= 1):
+        if threading.activeCount() <= 1:
             break
-        else:
-            pass
 
     print 'Scan End!'
